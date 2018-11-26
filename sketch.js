693b9c6ccd8a4eb146c03cf1216ca5fcba969551
RodrigoGamboa <33301343+RodrigoGamboa@users.noreply.github.com>
2018-11-26 07:27:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
 function setup() 
 {
   createCanvas(400, 400);
-  //canvas.position(0, 20);
 }
 
 function draw() 
 {
-  background(255);
-  fill(255, 0, 0);
-  rect(width/2, heigh/2, 50, 50);
+  background(255, 0, 0);
 }
